72f1ca41f9ccc5c12052af9af303a8ca48f5a3a8
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2017-03-13 01:42:33 -0400

    Removed sample code

--- GuitarExample/GuitarExample/AppDelegate.swift
+++ GuitarExample/GuitarExample/AppDelegate.swift
@@ -15,27 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func applicationDidFinishLaunching(_ application: UIApplication) {
-        let x = "HelloWorld"
-        print(x.splitWordsByCase())
 
-        let y = "helloWorld"
-        print(y.splitWordsByCase())
-
-        let z = "Hello_World"
-        print(z.splitWordsByCase())
-
-        let originalString = "NaCuHHe"
-        var newStringArray: [String] = []
-        for character in originalString.characters {
-            if String(character) == String(character).uppercased() {
-                newStringArray.append(" ")
-            }
-            newStringArray.append(String(character))
-        }
-
-        let newString = newStringArray.joined().trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .whitespaces).filter({ !$0.isEmpty }).joined(separator: " ")
-        print(newString)
-        
     }
 
 }
