1bcfcfb51faccc89bf9f41eafe34e2a43c125579
Hiroshi Kimura <muukii.app@gmail.com>
2019-08-14 06:17:55 +0900

    Update Package.swift (#25)

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Guitar",
+    products: [
+        .library(name: "Guitar", targets: ["Guitar"]),
+    ],
     targets: [
         .target(name: "Guitar", path: "Sources/"),
     ]   
