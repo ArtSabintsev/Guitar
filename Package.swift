1d3cc02873653ccd31b4d7b47b7e7760ab19afad
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-06-13 15:30:09 -0400

    Updated manifest

--- Package.swift
+++ Package.swift
@@ -1,15 +1,10 @@
-// swift-tools-version:5.1
-
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "Guitar",
-    products: [
-        .library(name: "Guitar", targets: ["Guitar"]),
-    ],
-    targets: [
-        .target(name: "Guitar", path: "Sources/"),
-    ],
     platforms: [.iOS(.v9)],  
+    products: [.library(name: "Guitar", targets: ["Guitar"])],
+    targets: [ .target(name: "Guitar", path: "Sources/")],
     swiftLanguageVersions: [.v5]   
 )
